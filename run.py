c1fe4ef2e6983b1e93b328fa1724cf3b752b56ae
jasonkimik <jasonkimik@users.noreply.github.com>
2018-05-04 15:31:08 -0400

    Update run.py

--- run.py
+++ run.py
@@ -4,6 +4,8 @@ from pyswip import Prolog
 prolog = Prolog()
 prolog.consult("project.pl")
 
+plmode = False
+
 # ===========================================================
 # Main loop:
 # 1. Repeat "input-response" cycle until input starts with "bye"
@@ -12,12 +14,21 @@ prolog.consult("project.pl")
 # 		1.2 Processing tokenized list
 # ===========================================================
 def chat():
-    while(True):
-        userInput = readinput()
-        if userInput[0].startswith("bye"):
-        	break
-        process(userInput)
-
+	while(True):
+		userInput = readinput()
+		if userInput[0].startswith("bye"):
+			print("Bye!")
+			break
+		elif userInput[0].startswith("plmode"):
+			plmode = True
+			continue
+		elif userInput[0].startswith("plend"):
+			plmode = False
+		print(userInput[0])
+		if plmode:
+			print(prolog.query(userInput[0]))
+		else:
+			process(userInput)
 # ===========================================================
 # Read input:
 # 1. Read a string from keyboard. 
